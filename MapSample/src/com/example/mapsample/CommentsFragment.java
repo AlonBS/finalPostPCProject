66aeefea47e80ec708d7ba08ee35029b105377e9
dror <drorcohe@walla.co.il>
2014-08-14 16:59:08 +0300

    added send button to comment fragment

--- MapSample/src/com/example/mapsample/CommentsFragment.java
+++ MapSample/src/com/example/mapsample/CommentsFragment.java
@@ -4,11 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 public class CommentsFragment extends Fragment{
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.comments_fragment,container, false);
+		
+		ShowDealActivity parentActivity = (ShowDealActivity)getActivity();
+		if(!parentActivity.isInUserMode){
+			
+			//comment_edit_text;
+		}
+		
 		return view;
 	}
 }
