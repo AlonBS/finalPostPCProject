98fa66a674f07a95e6dd8467a87b484b909fd08b
dror <drorcohe@walla.co.il>
2014-08-29 13:07:54 +0300

    added option to load images from camera

--- MapSample/src/com/dna/radius/businessmode/BusinessDashboardFragment.java
+++ MapSample/src/com/dna/radius/businessmode/BusinessDashboardFragment.java
@@ -4,6 +4,7 @@ package com.dna.radius.businessmode;
 import java.util.ArrayList;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
@@ -14,6 +15,7 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -40,7 +42,8 @@ public class BusinessDashboardFragment extends Fragment{
 	private ImageView imageView;
 
 	/**this variable is used for loading an image from the gallery*/
-	private final static int RESULT_LOAD_IMAGE = 1;
+	private final static int RESULT_LOAD_IMAGE_GALLERY = 1;
+	private final static int RESULT_LOAD_IMAGE_CAMERA = 2;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -52,7 +55,7 @@ public class BusinessDashboardFragment extends Fragment{
 		dbHandler = new DBHandler();
 		final TextView dealTv = (TextView) view.findViewById(R.id.deal_tv);
 		dealTv.setText(activityParent.ownerData.currentDeal);
-		
+
 		/*handles the image of the business*/
 		imageView = (ImageView)view.findViewById(R.id.buisness_image_view);
 		if(activityParent.ownerData.hasImage){
@@ -60,13 +63,30 @@ public class BusinessDashboardFragment extends Fragment{
 		}else{
 			imageView.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.set_business_image));
 		}
+
 		imageView.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View arg0) {
-				Intent i = new Intent(
-						Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-				startActivityForResult(i, RESULT_LOAD_IMAGE);
+				new AlertDialog.Builder(activityParent)
+				.setTitle("Choose an image for your business")
+				.setMessage("please choose an image source")
+				.setPositiveButton("Camera", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int whichButton) {
+						Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+						if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+							startActivityForResult(takePictureIntent, RESULT_LOAD_IMAGE_CAMERA);
+						}
+					}
+				}).setNegativeButton("Gallery", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int whichButton) {
+						Intent i = new Intent(
+								Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+						startActivityForResult(i, RESULT_LOAD_IMAGE_GALLERY);
+					}
+				}).show();
+
+
 			}
 		});
 
@@ -145,13 +165,15 @@ public class BusinessDashboardFragment extends Fragment{
 
 
 	/**
-	 * currently used for handling an image which was picked from the gallery by the user.
+	 * currently used for handling an image which was picked from the gallery by the user, or
+	 * an image which was took by the camera.
 	 */
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
+		Bitmap newBmap = null;
 		/**receives an image from the gallery, and change the image of the business*/
-		if (requestCode == RESULT_LOAD_IMAGE && resultCode == FragmentActivity.RESULT_OK && null != data) {
+		if (requestCode == RESULT_LOAD_IMAGE_GALLERY   && resultCode == FragmentActivity.RESULT_OK && null != data) {
 			Uri selectedImage = data.getData();
 			String[] filePathColumn = { MediaStore.Images.Media.DATA };
 
@@ -162,11 +184,22 @@ public class BusinessDashboardFragment extends Fragment{
 			int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
 			String picturePath = cursor.getString(columnIndex);
 			cursor.close();
+
+			newBmap = BitmapFactory.decodeFile(picturePath);
 			
-			Bitmap newBmap = BitmapFactory.decodeFile(picturePath);
-			activityParent.ownerData.changeBusinessImage(BitmapFactory.decodeFile(picturePath));
-			// String picturePath contains the path of selected Image
-			imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+		}
+		else if (requestCode == RESULT_LOAD_IMAGE_CAMERA  && resultCode == FragmentActivity.RESULT_OK) {
+	        Bundle extras = data.getExtras();
+	        newBmap = (Bitmap) extras.get("data");
+	    }else{
+	    	return;
+	    }
+		
+		if(newBmap!=null){
+			activityParent.ownerData.changeBusinessImage(newBmap);
+			imageView.setImageBitmap(newBmap);
+		}else{
+			Log.e("BusinessDashboardFragment", "ERROR!! The RETURNED BITMAP IS NULL");
 		}
 	}
 
