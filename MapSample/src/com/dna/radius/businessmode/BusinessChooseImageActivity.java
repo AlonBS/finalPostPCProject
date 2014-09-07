fe756707c09e9d98f897fa83dc68a047845360ae
dror <drorcohe@walla.co.il>
2014-09-07 12:26:13 +0300

    worked on the image loading segment in the business dashboard

---
+++ MapSample/src/com/dna/radius/businessmode/BusinessChooseImageActivity.java
@@ -0,0 +1,17 @@
+package com.dna.radius.businessmode;
+
+import android.os.Bundle;
+
+import com.dna.radius.R;
+import com.dna.radius.infrastructure.BaseActivity;
+
+public class BusinessChooseImageActivity extends BaseActivity{
+
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.business_choose_image_activity);
+		
+	}
+
+}
