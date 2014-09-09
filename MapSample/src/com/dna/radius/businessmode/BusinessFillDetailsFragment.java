89d71881a136c45190ad72cae63bf59e8794355a
dror <drorcohe@walla.co.il>
2014-09-09 23:18:44 +0300

    added icons to the spinner

--- MapSample/src/com/dna/radius/businessmode/BusinessFillDetailsFragment.java
+++ MapSample/src/com/dna/radius/businessmode/BusinessFillDetailsFragment.java
@@ -1,5 +1,7 @@
 package com.dna.radius.businessmode;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -50,7 +52,7 @@ public class BusinessFillDetailsFragment extends Fragment{
 		businessPhoneEditText = (EditText) view.findViewById(R.id.business_phone_textView);
 		businessAddressEditText = (EditText) view.findViewById(R.id.business_address_textView);
 		setBusinessTypeSpinner();
-		
+
 		if(changeCurrentSettingsMode){
 			businessNameEditText.setHint(businessNameHint);
 
@@ -65,7 +67,7 @@ public class BusinessFillDetailsFragment extends Fragment{
 			if(businessAddressHint!=null && !businessAddressHint.equals("")){
 				businessAddressEditText.setHint(businessAddressHint);
 			}
-			
+
 			//since we are on the settings menu, no message is requires
 			TextView welcomeMessage = (TextView)view.findViewById(R.id.welcome_business_textView);
 			welcomeMessage.setVisibility(View.GONE);
@@ -77,129 +79,159 @@ public class BusinessFillDetailsFragment extends Fragment{
 
 	private void setBusinessTypeSpinner() {
 
-		adapter = ArrayAdapter.createFromResource(getActivity(),
-				R.array.business_type_spinner, android.R.layout.simple_spinner_item);
-		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		ArrayList<SupportedTypes.BusinessType> businessTypes = new ArrayList<SupportedTypes.BusinessType>();
+		for(BusinessType b : BusinessType.values()){
+			businessTypes.add(b);
+		}
+		SpinnerTypeAdapter adapter = new SpinnerTypeAdapter(getActivity(), R.layout.spinner_types_layout, businessTypes,getResources());
 		businessTypeSpinner.setAdapter(adapter);
 
 		businessTypeSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
 
-			public void onItemSelected(AdapterView<?> parent, View view, 
-					int pos, long id) {
-
-				String typeStr = parent.getItemAtPosition(pos).toString();
+			@Override
+			public void onItemSelected(AdapterView<?> data, View view, int pos,
+					long id) {
+				// TODO Auto-generated method stub
+				String typeStr = data.getItemAtPosition(pos).toString();
 				businessType = SupportedTypes.BusinessType.stringToType(typeStr);
 
 				if (businessType == null) {
 					Log.e("BusinessWelcomeFillDerailsFragment", "error choosing a spinner option");
 				}
+				
 			}
 
-			public void onNothingSelected(AdapterView<?> parent) {
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				// TODO Auto-generated method stub
+				
 			}
-
 		});
+		
 
-	}
+			//		adapter = ArrayAdapter.createFromResource(getActivity(),
+			//				R.array.business_type_spinner, android.R.layout.simple_spinner_item);
+			//		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			//		businessTypeSpinner.setAdapter(adapter);
+			//
+			//		businessTypeSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+			//
+			//			public void onItemSelected(AdapterView<?> parent, View view, 
+			//					int pos, long id) {
+			//
+			//				String typeStr = parent.getItemAtPosition(pos).toString();
+			//				businessType = SupportedTypes.BusinessType.stringToType(typeStr);
+			//
+			//				if (businessType == null) {
+			//					Log.e("BusinessWelcomeFillDerailsFragment", "error choosing a spinner option");
+			//				}
+			//			}
+			//
+			//			public void onNothingSelected(AdapterView<?> parent) {
+			//			}
+			//
+			//		});
 
-	/**
-	 * return true iff the user filled all the relevant data
-	 * @return
-	 */
-	public boolean neededInfoGiven() {
-
-		String businessName = businessNameEditText.getText().toString();
-
-		if (businessName.isEmpty() || illegalBusinessName(businessName)) {
-			Toast.makeText(getActivity(), getResources().getString(R.string.business_name_forgot_to_fill), Toast.LENGTH_SHORT).show();
-			return false;
 		}
-		if (businessType == null) {
-			Toast.makeText(getActivity(), getResources().getString(R.string.business_type_forgot_to_fill), Toast.LENGTH_SHORT).show();
-			return false;
+
+		/**
+		 * return true iff the user filled all the relevant data
+		 * @return
+		 */
+		public boolean neededInfoGiven() {
+
+			String businessName = businessNameEditText.getText().toString();
+
+			if (businessName.isEmpty() || illegalBusinessName(businessName)) {
+				Toast.makeText(getActivity(), getResources().getString(R.string.business_name_forgot_to_fill), Toast.LENGTH_SHORT).show();
+				return false;
+			}
+			if (businessType == null) {
+				Toast.makeText(getActivity(), getResources().getString(R.string.business_type_forgot_to_fill), Toast.LENGTH_SHORT).show();
+				return false;
+			}
+
+			return true;
+
 		}
 
-		return true;
+		private boolean illegalBusinessName(String bn) {
+
+			//TODO business name filter? alpha-numeric only ? 
+			return false;
+
+		}
+
+		public String getBusinessName(){
+			return businessNameEditText.getText().toString();
+		}
+
+		public SupportedTypes.BusinessType getBusinessType(){
+			return businessType;
+		}
+
+		public String getBusinessPhoneNumber(){
+
+			return businessPhoneEditText.getText().toString();
+		}
+
+		public String getBusinessAddress(){
+
+			return businessAddressEditText.getText().toString();
+		}
+
+		/**
+		 * this function is called from the settings activity and is used to set a hint for the fragment.
+		 * the phoneNumber and address parameters can be null or empty strings. in this case, the hint won't change for these fields.
+		 */
+		public void setHint(String businessNameHint,SupportedTypes.BusinessType typeHint,  String businessPhoneHint, String businessAddressHint){
+			changeCurrentSettingsMode = true;
+			this.businessNameHint = businessNameHint;
+			this.typeHint = typeHint;
+
+			this.businessPhoneHint = businessPhoneHint;
+			this.businessAddressHint = businessAddressHint;
+
+		}
+
+		//TODO ODODODODOFDSOFOSADFO
+		//	/**
+		//	 * note - this function assumes that the setHint() function was called earlier.
+		//	 */
+		//	@Override
+		//	public void applyChangesIfNeeded() {
+		//
+		//		boolean didDataChanged = false;
+		//		//tests if the users changed the user name and changes it accordingly
+		//		String newBusinessName = businessNameEditText.getText().toString();
+		//		if(!newBusinessName.equals("") && !newBusinessName.equals(businessNameHint)){
+		//			didDataChanged = true;
+		//			//TODO change business Name to newBusinessName
+		//		}
+		//
+		//		BusinessType newBusinessType = businessType;
+		//		if(newBusinessType != typeHint){
+		//			didDataChanged = true;
+		//			//TODO change business Type to newBusinessType
+		//		}
+		//
+		//		String newAddress = businessAddressEditText.getText().toString();
+		//		if(!newAddress.equals("") && !newAddress.equals(businessAddressHint)){
+		//			didDataChanged = true;
+		//			//TODO change business address to newBusinessName
+		//		}
+		//
+		//		String newPhone = businessPhoneEditText.getText().toString();
+		//		if(!newPhone.equals("") && !newPhone.equals(businessPhoneHint)){
+		//			didDataChanged = true;
+		//			//TODO change business phone to newPhone
+		//		}
+		//
+		//		if(didDataChanged){
+		//			Toast.makeText(getActivity(), getResources().getString(R.string.data_changed_successfully), Toast.LENGTH_SHORT).show();
+		//		}
+		//
+		//	}
+
 
 	}
-
-	private boolean illegalBusinessName(String bn) {
-
-		//TODO business name filter? alpha-numeric only ? 
-		return false;
-
-	}
-
-	public String getBusinessName(){
-		return businessNameEditText.getText().toString();
-	}
-
-	public SupportedTypes.BusinessType getBusinessType(){
-		return businessType;
-	}
-
-	public String getBusinessPhoneNumber(){
-
-		return businessPhoneEditText.getText().toString();
-	}
-
-	public String getBusinessAddress(){
-
-		return businessAddressEditText.getText().toString();
-	}
-
-	/**
-	 * this function is called from the settings activity and is used to set a hint for the fragment.
-	 * the phoneNumber and address parameters can be null or empty strings. in this case, the hint won't change for these fields.
-	 */
-	public void setHint(String businessNameHint,SupportedTypes.BusinessType typeHint,  String businessPhoneHint, String businessAddressHint){
-		changeCurrentSettingsMode = true;
-		this.businessNameHint = businessNameHint;
-		this.typeHint = typeHint;
-
-		this.businessPhoneHint = businessPhoneHint;
-		this.businessAddressHint = businessAddressHint;
-
-	}
-
-//TODO ODODODODOFDSOFOSADFO
-//	/**
-//	 * note - this function assumes that the setHint() function was called earlier.
-//	 */
-//	@Override
-//	public void applyChangesIfNeeded() {
-//
-//		boolean didDataChanged = false;
-//		//tests if the users changed the user name and changes it accordingly
-//		String newBusinessName = businessNameEditText.getText().toString();
-//		if(!newBusinessName.equals("") && !newBusinessName.equals(businessNameHint)){
-//			didDataChanged = true;
-//			//TODO change business Name to newBusinessName
-//		}
-//
-//		BusinessType newBusinessType = businessType;
-//		if(newBusinessType != typeHint){
-//			didDataChanged = true;
-//			//TODO change business Type to newBusinessType
-//		}
-//
-//		String newAddress = businessAddressEditText.getText().toString();
-//		if(!newAddress.equals("") && !newAddress.equals(businessAddressHint)){
-//			didDataChanged = true;
-//			//TODO change business address to newBusinessName
-//		}
-//
-//		String newPhone = businessPhoneEditText.getText().toString();
-//		if(!newPhone.equals("") && !newPhone.equals(businessPhoneHint)){
-//			didDataChanged = true;
-//			//TODO change business phone to newPhone
-//		}
-//
-//		if(didDataChanged){
-//			Toast.makeText(getActivity(), getResources().getString(R.string.data_changed_successfully), Toast.LENGTH_SHORT).show();
-//		}
-//
-//	}
-
-
-}
---
+++ MapSample/src/com/dna/radius/businessmode/SpinnerTypeAdapter.java
@@ -0,0 +1,79 @@
+package com.dna.radius.businessmode;
+
+import java.util.ArrayList;
+
+import com.dna.radius.R;
+import com.dna.radius.infrastructure.SupportedTypes.BusinessType;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/***** Adapter class extends with ArrayAdapter ******/
+public class SpinnerTypeAdapter extends ArrayAdapter<BusinessType>{
+ 
+ private Activity activity;
+    private ArrayList<BusinessType> data;
+    public Resources res;
+    LayoutInflater inflater;
+ 
+ public SpinnerTypeAdapter(
+                 Activity activitySpinner, 
+                 int textViewResourceId,   
+                 ArrayList<BusinessType> data,
+                 Resources resLocal
+                ) 
+  {
+        super(activitySpinner, textViewResourceId, data);
+        this.activity = activitySpinner;
+        this.data     = data;
+        
+        data.add(0,BusinessType.ACCOMMODATION);
+        this.inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        
+   }
+
+    @Override
+    public View getDropDownView(int position, View convertView,ViewGroup parent) {
+        return getCustomView(position, convertView, parent);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return getCustomView(position, convertView, parent);
+    }
+
+    // This funtion called for each row ( Called data.size() times )
+    public View getCustomView(int position, View convertView, ViewGroup parent) {
+
+     /********** Inflate spinner_rows.xml file for each row ( Defined below ) ************/
+        View row = inflater.inflate(R.layout.spinner_types_layout, parent, false);
+        
+        /***** Get each Model object from Arraylist ********/
+         BusinessType currentType = (BusinessType) data.get(position);
+        
+        TextView label        = (TextView)row.findViewById(R.id.spinner_item_text_view);
+        ImageView icon = (ImageView)row.findViewById(R.id.spinner_item_image_view);
+        
+        if(position==0){
+         
+         // Default selected Spinner item 
+         label.setText(activity.getResources().getString(R.string.business_type));
+        }
+        else
+        {
+            // Set values for spinner each row 
+            label.setText(currentType.getStringRep());
+            icon.setImageResource(currentType.getIconID());
+            
+        }   
+
+        return row;
+      }
+ }
