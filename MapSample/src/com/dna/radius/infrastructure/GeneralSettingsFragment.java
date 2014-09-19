b0cb2969d1875c21c09d3ae082d3c40a664339a6
dror <drorcohe@walla.co.il>
2014-09-19 14:16:47 +0300

    added delete user button
    fixed design in client mode
    fixed design in deal history

--- MapSample/src/com/dna/radius/infrastructure/GeneralSettingsFragment.java
+++ MapSample/src/com/dna/radius/infrastructure/GeneralSettingsFragment.java
@@ -1,10 +1,14 @@
 package com.dna.radius.infrastructure;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.dna.radius.R;
@@ -17,6 +21,7 @@ public class GeneralSettingsFragment extends Fragment{
 	private EditText newPasswordEditText;
 	private EditText newPasswordConformationEditText;
 	private EditText emailEditText;
+	private Button deleteAcountButton;
 	
 	public static String USER_NAME_PARAM = "uesrNameParameter";
 	public static String EMAIL_PARAM = "emailParameter";
@@ -28,11 +33,31 @@ public class GeneralSettingsFragment extends Fragment{
 		newPasswordEditText = (EditText)view.findViewById(R.id.password_edit_text);
 		newPasswordConformationEditText = (EditText)view.findViewById(R.id.password_confirmation_edit_text);
 		emailEditText = (EditText)view.findViewById(R.id.email_address_edit_text);
-
+		deleteAcountButton = (Button)view.findViewById(R.id.delete_acount_button);
+		
 		userNameEditText.setOnTouchListener(new BusinessSettingsActivity.EditTextOnTouchListenerWithinTabhost());
 		newPasswordEditText.setOnTouchListener(new BusinessSettingsActivity.EditTextOnTouchListenerWithinTabhost());
 		newPasswordConformationEditText.setOnTouchListener(new BusinessSettingsActivity.EditTextOnTouchListenerWithinTabhost());
 		emailEditText.setOnTouchListener(new BusinessSettingsActivity.EditTextOnTouchListenerWithinTabhost());
+		deleteAcountButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				new AlertDialog.Builder(getActivity())
+				.setTitle(getResources().getString(R.string.switch_mode))
+				.setMessage("are you sure you want to delete your acount?")
+				.setNegativeButton("No", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int whichButton) {
+					}
+				}).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int whichButton) {
+						//TODO - ALON - do your magic
+						getActivity().finish();
+					}
+				}).show();
+				
+			}
+		});
 		
 		
 		String currentUserName = getArguments().getString(USER_NAME_PARAM);
