38b705392439099459c715c81227e16f910aa826
dror <drorcohe@walla.co.il>
2014-10-11 10:57:09 +0300

    small fix - google analytics

--- MapSample/src/com/dna/radius/login/MainActivity.java
+++ MapSample/src/com/dna/radius/login/MainActivity.java
@@ -143,17 +143,6 @@ public class MainActivity extends BaseActivity {
 		return true;
 	}
 	
-	@Override
-	protected void onStart() {
-		super.onStart();
-		GoogleAnalytics.getInstance(this).reportActivityStart(this);
-	}
-	
-	@Override
-	protected void onStop() {
-		super.onStop();
-		GoogleAnalytics.getInstance(this).reportActivityStop(this);
-	}
 
 
 }
